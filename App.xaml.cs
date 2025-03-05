1e691970199ec2d691251ee3fc21ddb402bf2769
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2025-03-05 07:50:47 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
