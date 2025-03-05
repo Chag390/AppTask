0e0fcdbf2dbd99437627e4a3407116d2d8c75f00
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2025-03-05 10:00:29 -0300

    Criação das paginas StartPage e AddEditPage, Informada para a Pagina App a pagina StartPage com pagina inicial . Excluxão da MainPage

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,20 @@
-﻿namespace AppTask
+﻿using AppTask.Views;
+
+namespace AppTask
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            //MainPage = new NavigationPage(new StartPage()); 
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage());
+            return new Window(new StartPage());
+            
         }
     }
 }
