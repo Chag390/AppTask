12e1d2c54bf3d0313e9ba0cd1e4e5a7f7aad214c
Cláudio Gomes <claudio.chag@gmail.com>
2025-03-05 21:00:10 -0300

    Removido a Borda do DataPicker

--- App.xaml.cs
+++ App.xaml.cs
@@ -23,6 +23,13 @@ namespace AppTask
 
         private void CustomHandler()
         {
+            EntryNoBorder();
+            DatePickerNoBorder();
+
+        }
+
+        private static void EntryNoBorder()
+        {
             Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
             {
                 //Condicionais de compilação
@@ -31,15 +38,34 @@ namespace AppTask
                     handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
 #endif
 #if IOS || MACCATALYST
-                    //IOS || MACCATALYST
-                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+                //IOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
 #endif
 #if WINDOWS
                 // WINDOWS não funciona 100%
                 handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
 #endif
             });
+        }
 
+        private static void DatePickerNoBorder()
+        {
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+                //Condicionais de compilação
+#if ANDROID
+                    //ANDROID
+                    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+#endif
+#if IOS || MACCATALYST
+                //IOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+#if WINDOWS
+                // WINDOWS não funciona 100%
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
         }
     }            
     
