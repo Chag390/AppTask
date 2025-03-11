a9f2b08f8be0b868132c146627a714bfe8ba941f
claudio.chag@hotmail.com <claudio.chag@hotmail.com>
2025-03-11 08:49:19 -0300

    configurção do DBContext

---
+++ SubTaskModel.cs
@@ -0,0 +1,9 @@
+namespace AppTask.Models
+{
+    public class SubTaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
